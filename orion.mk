cab365b81a3e5c15e74bf3f5120be7b7be3da3c6
mosimchah <mosimchah@gmail.com>
2016-09-02 16:04:11 -0400

    rename device to "n1awifi"
    Change-Id: I470102de7f5d4967eeb332fb597cca389e4c85c7

--- orion.mk
+++ orion.mk
@@ -3,11 +3,11 @@ $(call inherit-product, vendor/orion/config/common_full_tablet_wifionly.mk)
 
 #Release name
 PRODUCT_RELEASE_NAME :=SAMSUNG SM-P600
-PRODUCT_MODEL :=lt03wifi
-PRODUCT_NAME :=orion_lt03wifi
+PRODUCT_MODEL :=n1awifi
+PRODUCT_NAME :=orion_n1awifi
 PRODUCT_MANUFACTURER :=samsung
 
-$(call inherit-product, device/samsung/lt03wifi/full_lt03wifi.mk)
+$(call inherit-product, device/samsung/n1awifi/full_n1awifi.mk)
 
     # Device maintainer
 PRODUCT_BUILD_PROP_OVERRIDES := DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
