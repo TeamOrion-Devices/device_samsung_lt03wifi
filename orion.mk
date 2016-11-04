5c2aa280243c769f3acb8801a6dc68eea61a364c
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-03 22:07:11 -0500

    Use Exynos5420-common
    Change-Id: I219d8bd6062fd97c3e428313c46b48298bcfabde

--- orion.mk
+++ orion.mk
@@ -1,3 +1,9 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from n1awifi device
+$(call inherit-product, device/samsung/n1awifi/device.mk)
+
 # Inherit some common orion stuff.
 $(call inherit-product, vendor/orion/config/common_full_tablet_wifionly.mk)
 
@@ -7,7 +13,11 @@ PRODUCT_MODEL :=n1awifi
 PRODUCT_NAME :=orion_n1awifi
 PRODUCT_MANUFACTURER :=samsung
 
-$(call inherit-product, device/samsung/n1awifi/full_n1awifi.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-P600 \
+    PRODUCT_NAME=n1awifi \
+    PRODUCT_DEVICE=n1awifi \
+    TARGET_DEVICE=n1awifi
 
     # Device maintainer
 PRODUCT_BUILD_PROP_OVERRIDES := DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
